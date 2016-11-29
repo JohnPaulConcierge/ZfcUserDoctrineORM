b0b55d255182b605d0bdb79f3d27f3a5b445d39f
Gary Gitton <gary.gitton@gmail.com>
2016-11-29 20:09:18 +0100

    implement interface

--- src/ZfcUserDoctrineORM/Mapper/User.php
+++ src/ZfcUserDoctrineORM/Mapper/User.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use ZfcUser\Mapper\User as ZfcUserMapper;
 use ZfcUserDoctrineORM\Options\ModuleOptions;
 use Zend\Stdlib\Hydrator\HydratorInterface;
+use ZfcUser\Entity\UserInterface;
 
 class User extends ZfcUserMapper
 {
@@ -46,12 +47,12 @@ class User extends ZfcUserMapper
         return $er->find($id);
     }
 
-    public function insert($entity, $tableName = null, HydratorInterface $hydrator = null)
+    public function insert(UserInterface $entity, $tableName = null, HydratorInterface $hydrator = null)
     {
         return $this->persist($entity);
     }
 
-    public function update($entity, $where = null, $tableName = null, HydratorInterface $hydrator = null)
+    public function update(UserInterface $entity, $where = null, $tableName = null, HydratorInterface $hydrator = null)
     {
         return $this->persist($entity);
     }
