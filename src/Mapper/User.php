8ba583c44f7f77f228377e10813dbe05bbd672b9
Florent Blaison <florent.blaison@gmail.com>
2017-09-20 18:04:14 +0200

    Update User.php

--- src/Mapper/User.php
+++ src/Mapper/User.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use ZfcUser\Mapper\User as ZfcUserMapper;
 use ZfcUserDoctrineORM\Options\ModuleOptions;
 use Zend\Stdlib\Hydrator\HydratorInterface;
-use ZfcUser\Entity\UserInterface;
 
 class User extends ZfcUserMapper
 {
@@ -47,12 +46,12 @@ class User extends ZfcUserMapper
         return $er->find($id);
     }
 
-    public function insert(UserInterface $entity, $tableName = null, HydratorInterface $hydrator = null)
+    public function insert($entity, $tableName = null, HydratorInterface $hydrator = null)
     {
         return $this->persist($entity);
     }
 
-    public function update(UserInterface $entity, $where = null, $tableName = null, HydratorInterface $hydrator = null)
+    public function update($entity, $where = null, $tableName = null, HydratorInterface $hydrator = null)
     {
         return $this->persist($entity);
     }
